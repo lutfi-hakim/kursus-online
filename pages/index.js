7f87e8f8148930a14f341487963d39f2f9005049
lutfi-hakim <21lutfihakim@gmail.com>
2021-02-20 22:12:03 +0700

    setting nextjs tailwindcss

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto mt-4">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>halaman utama</main>
+    </div>
+  );
+}
